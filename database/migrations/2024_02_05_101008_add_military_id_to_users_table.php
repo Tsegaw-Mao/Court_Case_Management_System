17135f73fbf107312e8eb0bdbd28b5712e2f9cb8
Alex-fite <alexfite430@gmail.com>
2024-02-05 10:30:41 +0000

    upadting user table

---
+++ database/migrations/2024_02_05_101008_add_military_id_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('militaryId')->unique();
+            $table->string('rank');
+            $table->string('branch');
+            $table->string('unit');
+            $table->string('active');
+            // $table->timestamp('updated_at')->nullable();
+
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};

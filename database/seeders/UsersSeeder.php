427318a87260559e3a95557bbc0b18459e8c7f96
Tsegaw-Mao <tsegawjohnj@gmail.com>
2024-02-17 19:00:09 +0300

    saturday commit

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $name = [
+            'alemu',
+            'abreham',
+            'ashenafi',
+            'bereket',
+            'beza',
+            'brukti',
+            'chernet',
+            'degega',
+            'dawit',
+            'daniel',
+            'elias',
+            'efrem',
+            'frew',
+            'gizachew',
+            'hiwot',
+            'henok',
+            'isayas',
+            'jemal',
+            'kefyalew',
+            'lemlem',
+            'meseret',
+            'netsanet',
+            'rediet',
+            'sisay',
+            'tesfaye',
+            'omar',
+            'mustefa',
+            'wondosen',
+            'yared',
+            'yafet',
+            'zelalem',
+            'zerihun',
+            'zewditu'
+        ];
+        for ($i = 0; $i < count($name); $i++) {
+            $uid = $name[$i] . '001';
+            $email = $name[$i] . '@gmail.com';
+            User::create([
+                'name' => $name[$i],
+                'UserId' => $uid,
+                'email' => $email,
+                'password' => Hash::make($email)
+            ]);
+        }
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,17 +14,33 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <link href="{{asset('assets/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-black shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                  Mod CCMS 
+                    Mod CCMS
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -37,44 +54,36 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            @canany(['create-role', 'edit-role', 'delete-role'])
-                                <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Roles</a></li>
-                            @endcanany
-                            @canany(['create-user', 'edit-user', 'delete-user'])
-                                <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
-                            @endcanany
-                            @canany(['create-product', 'edit-product', 'delete-product'])
-                                <li><a class="nav-link" href="{{ route('products.index') }}">Manage Products</a></li>
-                            @endcanany
+                        @canany(['create-role', 'edit-role', 'delete-role'])
+                        <li><a class="nav-link" href="{{ route('admin.home') }}">Home</a></li>
+                        @endcanany
+                        <div class='float-end'>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a class="dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
                                     {{ Auth::user()->name }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <ul class="dropdown-menu">
+                                    <li><a class="get-started-btn scrollto dropdown-item"
+                                            href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">Logout</a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                            @csrf
+                                        </form>
+                                    </li>
+                        </div>
                         @endguest
                     </ul>
                 </div>
@@ -85,22 +94,22 @@
             <div class="container">
                 <div class="row justify-content-center mt-3">
                     <div class="col-md-12">
-                        
+
                         @if ($message = Session::get('success'))
-                            <div class="alert alert-success text-center" role="alert">
-                                {{ $message }}
-                            </div>
+                        <div class="alert alert-success text-center" role="alert">
+                            {{ $message }}
+                        </div>
                         @endif
 
                         <h3 class="text-center mt-3 mb-3">Ministry of Defence Court Case management System</h3>
                         @yield('content')
-                        
+
                         <div class="row justify-content-center text-center mt-3">
                             <div class="col-md-12">
-                                <p>For any help contact us: 
+                                <p>For any help contact us:
                                     <a href="https://www.govModCCMS.com/"><strong>Contac link</strong></a>
                                 </p>
-                                
+
                             </div>
                         </div>
 
@@ -111,4 +120,5 @@
         </main>
     </div>
 </body>
+
 </html>

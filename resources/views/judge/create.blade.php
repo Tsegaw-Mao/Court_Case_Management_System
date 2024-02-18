d20023318ab382a9a95069aa0d405dc1460c7b84
Tsegaw-Mao <tsegawjohnj@gmail.com>
2024-02-18 09:41:04 +0300

    sunday 3.2

--- resources/views/judge/create.blade.php
+++ resources/views/judge/create.blade.php
@@ -1,20 +1,21 @@
 @extends('master1')
 
 @section('body')
-@if (session('status'))
-<div class="alert alert-success">{{session('status')}}</div>
-@endif
 <div class="card">
-        <div class="card-header bold"> Judge Registration</div>
-        <div class="card-body">
-            <form action="{{route('judge.store')}}" method="post">
+    @if (session('status'))
+    <div class="alert alert-success">{{session('status')}}</div>
+    @endif
+    <div class="card-header bold"> Judge Registration</div>
+    <div class="card-body">
+        <form action="{{route('judge.store')}}" method="post">
             @csrf
-                <div class="row">
+            <div class="row">
                 <div class="col">
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-0 col-sm-1 col-form-label bold">User Id:</label>
                         <div class="col-lg-4 col-md-4 col-sm-10">
-                            <input name="id" value="{{ old('UserId') }}" type="text" class="form-control" placeholder ="please enter user Id"required>
+                            <input name="id" value="{{ old('UserId') }}" type="text" class="form-control"
+                                placeholder="please enter user Id" required>
                         </div>
                     </div>
                 </div>
@@ -25,7 +26,8 @@
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-0 col-sm-1 col-form-label bold">First Name:</label>
                         <div class="col-lg-4 col-md-4 col-sm-10">
-                            <input name="firstName" value="{{ old('first') }}" type="text" class="form-control"placeholder="Enter your first name" required>
+                            <input name="firstName" value="{{ old('first') }}" type="text" class="form-control"
+                                placeholder="Enter your first name" required>
                         </div>
                     </div>
                 </div>
@@ -37,7 +39,8 @@
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-0 col-sm-1 col-form-label bold">Last Name:</label>
                         <div class="col-lg-4 col-md-4 col-sm-10">
-                            <input name="lastName" value="{{ old('last') }}" type="text" class="form-control"placeholder="Enter your last name">
+                            <input name="lastName" value="{{ old('last') }}" type="text" class="form-control"
+                                placeholder="Enter your last name">
                         </div>
                     </div>
                 </div>
@@ -49,7 +52,8 @@
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-0 col-sm-1 col-form-label bold">Email:</label>
                         <div class="col-lg-4 col-md-4 col-sm-10">
-                            <input name="email" value="{{ old('email') }}" type="email" class="form-control"placeholder="Enter your email" required>
+                            <input name="email" value="{{ old('email') }}" type="email" class="form-control"
+                                placeholder="Enter your email" required>
                         </div>
                     </div>
                 </div>
@@ -60,15 +64,16 @@
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-0 col-sm-1 col-form-label bold">Adress:</label>
                         <div class="col-lg-4 col-md-4 col-sm-10">
-                            <input name="address" value="{{ old('mobile') }}" type="text" class="form-control"placeholder="Enter your Mobile Number" maxlength="10">
+                            <input name="address" value="{{ old('mobile') }}" type="text" class="form-control"
+                                placeholder="Enter your Mobile Number" maxlength="10">
                         </div>
                     </div>
                 </div>
             </div>
 
-                <button center type="submit" class="btn btn-primary bold">Submit</button>
+            <button center type="submit" class="btn btn-primary bold">Submit</button>
 
-                <!-- <script defer
+            <!-- <script defer
                 src="https://unpkg.com/verifalia-widget@1.10.0/dist/verifalia-widget.js"
                 data-verifalia-appkey="YOUR BROWSER APP KEY HERE"
                 integrity="sha512-Byn2sawjqjYOq9vwcSZdb8ncm3h3B4EmeyQ5hM035sShmwzv58/LdwACNmzJnkmILKvEvavSvdI6HaHy8FJWvg=="
@@ -76,10 +81,10 @@
               <div style="display: none">Powered by Verifalia <a href="https://verifalia.com/">email verifier</a></div> -->
 
 
-                </div>
-            </form>
-        </div>
     </div>
+    </form>
+</div>
+</div>
 </div>
 
 @endsection
